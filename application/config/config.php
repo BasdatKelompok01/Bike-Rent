26595a598fa56e03b17c6aa49d2418d07f9977c7
BasdatKelompok01 <49897793+BasdatKelompok01@users.noreply.github.com>
2019-05-06 22:23:15 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://bikerent.ga/';
+$config['base_url'] = 'http://localhost/TK4_EXT_1';
 
 /*
 |--------------------------------------------------------------------------
